360d3531cd0f2b4fbd8c7c4dcf061ff32b886b0c
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-30 20:05:20 +0200

    → Checkpoint at: 20:52:05

--- source/pages/Kinoafisha.js
+++ source/pages/Kinoafisha.js
@@ -76,6 +76,7 @@ export class Kinoafisha extends Component {
                         </div>
                     </div>
                 </div>
+                <div className = 'content'>{moviesJSX}</div>
             </>
         );
     }
