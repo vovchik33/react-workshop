a3a94bbd9ef504e1fdfa645d66c559ee93cc26c0
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-30 16:18:47 +0200

    → Checkpoint at: 16:18:50

--- scripts/git/sync.js
+++ scripts/git/sync.js
@@ -55,7 +55,6 @@ const sync = (async function*() {
                         prune:     1,
                         callbacks: {
                             credentials(url, userName) {
-                                console.log('→ url', url, userName);
 
                                 return git.Cred.sshKeyFromAgent(userName);
                             },
