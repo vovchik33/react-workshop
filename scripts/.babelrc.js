545a2f15c972b4f0da39a110e8d239c23bd4b9e7
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-10-31 13:57:46 +0200

    → Checkpoint at: 13:57:47

--- scripts/.babelrc.js
+++ scripts/.babelrc.js
@@ -5,7 +5,7 @@ module.exports = api => {
 
     const plugins = [
         // Included until Node v.10 release (async generators)
-        '@babel/plugin-proposal-async-generator-functions',
+        // '@babel/plugin-proposal-async-generator-functions',
         '@babel/plugin-proposal-class-properties',
         'dynamic-import-node',
     ];
